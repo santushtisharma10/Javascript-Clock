76e74765126c27f9b670c7eda4a2fb31cf3f9e76
santushtisharma10 <sharmasantushti1012@gmail.com>
2021-09-18 16:22:13 +0530

    functionality added

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 setInterval(getTime, 1000)
 
+const minHand = document.querySelector("[data-minute]")
+const secHand = document.querySelector("[data-second]")
+const hourHand = document.querySelector("[data-hour]")
+
 function getTime() {
 
     const currDate = new Date()
-    const sec = currDate.getSeconds()
-    const min = currDate.getMinutes()
-    const hour = currDate.getHours()
-    console.log(currDate)
-    console.log(hour, min, sec)
+    const sec = currDate.getSeconds()/60
+    const min = (sec+currDate.getMinutes())/60
+    const hour = (min+currDate.getHours())/12
+
+    setRotation(hourHand, hour)
+    setRotation(minHand, min)
+    setRotation(secHand, sec)
+    
 }
 
-function setRotation() {
-
+function setRotation(element, rotationVal) {
     
+    //console.log("rotation value", 0.5*360)
+    element.style.setProperty('--hand-rotate', rotationVal * 360)
 }
 
