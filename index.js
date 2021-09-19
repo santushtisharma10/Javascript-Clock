3e032a587489427828e17e372951ac969be0226f
santushtisharma10 <sharmasantushti1012@gmail.com>
2021-09-19 13:48:01 +0530

    changes made

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ setInterval(getTime, 1000)
 const minHand = document.querySelector("[data-minute]")
 const secHand = document.querySelector("[data-second]")
 const hourHand = document.querySelector("[data-hour]")
+const time = document.getElementById("timeshow")
 
 function getTime() {
 
@@ -10,6 +11,9 @@ function getTime() {
     const sec = currDate.getSeconds()/60
     const min = (sec+currDate.getMinutes())/60
     const hour = (min+currDate.getHours())/12
+
+    console.log(currDate, time)
+    time.innerHTML = currDate
 
     setRotation(hourHand, hour)
     setRotation(minHand, min)
@@ -20,6 +24,12 @@ function getTime() {
 function setRotation(element, rotationVal) {
     
     //console.log("rotation value", 0.5*360)
+    
     element.style.setProperty('--hand-rotate', rotationVal * 360)
 }
 
+getTime()
+
+//Future Scope
+//Digital Clock
+// dark mode
